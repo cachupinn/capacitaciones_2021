7182f9cabed6414b7c94c1a54c0f5567e128d2c2
Sofia Lopez <sofia.lopez.g@ug.uchile.cl>
2021-10-22 21:47:46 -0300

    patito rojo

--- cap4/emergency_stop.py
+++ cap4/emergency_stop.py
@@ -29,8 +29,22 @@ def mov_duckiebot(key):
 def det_duckie(obs):
     ### DETECTOR HECHO EN LA MISIÓN ANTERIOR
     dets = list()
-
+    
+    lower_yellow = np.array([150, 100, 0])
+    upper_yellow = np.array([255, 255, 70])
+    
+    # Filtrar colores de la imagen en el rango utilizando
+    mask = cv2.inRange(obs, lower_yellow, upper_yellow)
+        
+    # Busca contornos de blobs
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
+        # Obtener rectangulo que bordea un contorno
+        x, y, w, h = cv2.boundingRect(cnt)
+        
+        #Filtrar por area minima
+        AREA = x * h   
+        min_area = 2500  
 
         if AREA > min_area:
             # En lugar de dibujar, se agrega a la lista
@@ -48,7 +62,7 @@ def draw_dets(obs, dets):
     return obs
 
 def red_alert(obs):
-    red_img = np.zeros((480, 640, 3), dtype = np.uint8)
+    red_img = np.zeros(obs.shape, dtype = np.uint8)
     red_img[:,:,0] = 255
     blend = cv2.addWeighted(obs, 0.5, red_img, 0.5, 0)
 
@@ -80,8 +94,6 @@ if __name__ == '__main__':
     # Posición del pato en el mapa (fija)
     duck_pos = np.array([2,0,2])
 
-    # Constante que se debe calcular
-    C = 1 # f * dr (f es constante, dr es conocido)
 
     while True:
 
@@ -90,36 +102,48 @@ if __name__ == '__main__':
         # Si la tecla es Esc, se sale del loop y termina el programa
         if key == 27:
             break
-
+        
         # Se define la acción dada la tecla presionada
         action = mov_duckiebot(key)
 
         # Si hay alerta evitar que el Duckiebot avance
         if alert:
-            pass
-
+            if key == ord("w"):
+                action.update(key, np.array([0.0, 0.0]))
+                # action = { **action, key: np.array([0.0, 0.0])}
+                # action[key] = 0
+                
         # Se ejecuta la acción definida anteriormente y se retorna la observación (obs),
         # la evaluación (reward), etc
         obs, reward, done, info = env.step(action)
 
         # Detección de patos, retorna lista de detecciones
-
+        dets = det_duckie(obs)
         # Dibuja las detecciones
-
+        obs = draw_dets(obs, dets)
         # Obtener posición del duckiebot
         dbot_pos = env.cur_pos
         # Calcular distancia real entre posición del duckiebot y pato
         # esta distancia se utiliza para calcular la constante
-        dist = CALCULAR
-
+        dist = 2*((dbot_pos[0] - duck_pos[0])**2 + (dbot_pos[1] - duck_pos[1])**2 + (dbot_pos[2] - duck_pos[2])**2)**1/2
+        
         # La alerta se desactiva (opción por defecto)
         alert = False
         
         for d in dets:
             # Alto de la detección en pixeles
-            p = DEFINIR
+            p = d[3]     
+            
+            # Constante que se debe calcular    
+            # C = f * hr (f es constante, hr es conocido)
+            # hr = 0.08 height del pato
+            # f = (p * Dr) / hr 
+            f = (p * dist) / 0.08
+            # Entonces 
+            C = f * 0.08
+
             # La aproximación se calcula según la fórmula mostrada en la capacitación
-            d_aprox = DEFINIR
+            d_aprox = C / p
 
             # Muestra información relevante
             print('p:', p)
@@ -127,10 +151,12 @@ if __name__ == '__main__':
             print('Dr:', dist)
 
             # Si la distancia es muy pequeña activa alerta
-            if d_aprox < 0.3:
-                # Activar alarma
-
+            if dist < 0.1: #d_aprox
                 # Muestra ventana en rojo
+                obs = red_alert(obs)
+                # Activar alarma
+                alert = True
+                
 
         # Se muestra en una ventana llamada "patos" la observación del simulador
         cv2.imshow('patos', cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
